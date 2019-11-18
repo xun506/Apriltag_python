7bba877a27067e358e6c54716ab16a0429db57ef
圣杰 朱 <1134940288@qq.com>
2019-11-18 14:25:36 +0800

    添加项目文件。

---
+++ Apriltag_python/Apriltag_python.py
@@ -0,0 +1,13 @@
+import cv2
+from apriltag import Apriltag
+
+ap = Apriltag()
+ap.create_detector(debug=True)
+filename = r'tag.png'
+frame = cv2.imread(filename)
+detections = ap.detect(frame)
+if len(detections) > 0:
+    print('识别成功')
+else:
+    print('识别失败')
+
